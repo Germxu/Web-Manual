f89660b5946e688fd17f1582a212ff49d3405331
Germxu <germxu@gmail.com>
2021-07-19 22:03:47 +0800

    isArray polyfill

--- MST/数组判断.js
+++ MST/数组判断.js
@@ -21,3 +21,11 @@ let arr = []
     Array.isArray(arr)
 
 // 其中方法1，2，3 主要是通过原型去判断的， 4是通过object类型的副属性class去判断的，其中函数的class是Function，结果是[object Function]， 普通的对象是Object，结果是[object Object]，5是es6新增的方法
+
+
+// ES6 isArray polyfill
+if (!Array.isArray) {
+    Array.isArray = function(arg) {
+      return Object.prototype.toString.call(arg) === '[object Array]';
+    };
+  }
